3826e7e07e2045190e3ddafeedb5cc92f629749c
Vivian Zhang <vvynz12@gmail.com>
2023-01-19 18:01:20 -0500

    Write a test to test 1/3 features on the homepage

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,13 @@
+import { eq } from "lodash"
+
 describe('home page', () => {
   it('the h1 contains the correct text', () => {
     cy.visit('http://localhost:3000')
     cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
+  })
+
+  it("the features on the homepage are correct", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("dt").eq(0).contains("4 Courses")
   })
 })
