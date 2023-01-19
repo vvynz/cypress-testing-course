0ba3c523d553d888fea807ed8fac8702c8b661e6
Vivian Zhang <vvynz12@gmail.com>
2023-01-19 18:05:52 -0500

    Update 2nd test so that it checks for the other 2/3 features on the homepage

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -6,8 +6,10 @@ describe('home page', () => {
     cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
   })
 
-  it("the features on the homepage are correct", () => {
+  it.only("the features on the homepage are correct", () => {
     cy.visit("http://localhost:3000")
     cy.get("dt").eq(0).contains("4 Courses")
+    cy.get("dt").eq(1).contains("25+ Lessons")
+    cy.get("dt").eq(2).contains("Free and Open Source")
   })
 })
