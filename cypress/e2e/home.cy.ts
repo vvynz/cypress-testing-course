55413f771f0e2308cb2ffd866e8151519d96063b
Vivian Zhang <vvynz12@gmail.com>
2023-01-19 17:40:28 -0500

    Update get selector

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,6 @@
 describe('home page', () => {
   it('the h1 contains the correct text', () => {
     cy.visit('http://localhost:3000')
-    cy.get("h1").contains("Testing Next.js Applications with Cypress")
+    cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
   })
 })
