27d15f6f75d9c2465bc33bfceebd96e808366a2d
Vivian Zhang <vvynz12@gmail.com>
2023-01-23 17:03:35 -0500

    Use context method to group all previous tests together under the hero section

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,17 +1,19 @@
-import { eq } from "lodash"
-
 describe('home page', () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000")
   })
-
-  it('the h1 contains the correct text', () => {
-    cy.getByData('hero-heading').contains("Testing Next.js Applications with Cypress")
+  
+  context("Hero section", () => {
+    it('the h1 contains the correct text', () => {
+      cy.getByData('hero-heading').contains("Testing Next.js Applications with Cypress")
+    })
+    
+    it("the features on the homepage are correct", () => {
+      cy.get("dt").eq(0).contains("4 Courses")
+      cy.get("dt").eq(1).contains("25+ Lessons")
+      cy.get("dt").eq(2).contains("Free and Open Source")
+    })
   })
 
-  it("the features on the homepage are correct", () => {
-    cy.get("dt").eq(0).contains("4 Courses")
-    cy.get("dt").eq(1).contains("25+ Lessons")
-    cy.get("dt").eq(2).contains("Free and Open Source")
-  })
+  
 })
